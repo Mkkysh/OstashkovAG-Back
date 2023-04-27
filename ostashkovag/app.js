ef6698fe636f5531b28f6ee85bdc602f4afa33d5
Mkkysh <kudrya123124@mail.ru>
2023-04-28 01:17:26 +0300

    add getEvents

--- ostashkovag/app.js
+++ ostashkovag/app.js
@@ -20,18 +20,38 @@ const config = {
 
 const client = new pg.Client(config);
 
-app.get('/api/getEvents' , jsonParser, (request, response) => {
-    var query = `SELECT * From public."Event";`
+app.post('/api/getEvents' , jsonParser, (request, response) => {
+    var {page, sort} = request.body;
+    page = !page ? 0 : page;
+    sort = !sort ? "DESC" : sort;
+    const countEvents = 2;
+    var countPage;
+
+    var query = `SELECT * From public."Event"
+                WHERE isarchive = false
+                ORDER BY datebegin ${sort}
+                LIMIT ${countEvents}
+                OFFSET ${countEvents*page};`;
+    
+    var queryCount = `SELECT * From public."Event"
+    WHERE isarchive = false;`;
+
+    client.query(queryCount, (err, res)=>{
+        if(err) {
+                console.log(err);
+                return;
+        }
+        countPage = Math.ceil(res.rowCount/countEvents);
+    })
+
     client.query(query, (err, res)=>{
         if(err) {
             console.log(err);
             return;
         }
-        response.status(200).send(res.rows)
+        response.status(200).send({events: res.rows, countPage: countPage});
     })
 });
-
-
 
 function main() {
     try{
