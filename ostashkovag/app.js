9669c5b2e79e743c7d28d75a760575acc175add5
Mkkysh <kudrya123124@mail.ru>
2023-04-29 23:26:36 +0300

    add getNews

--- ostashkovag/app.js
+++ ostashkovag/app.js
@@ -8,6 +8,28 @@ const jsonParser = express.json();
 const PORT = 3000;
 const app = express();
 
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    file.originalname = Buffer.from(file.originalname, "latin1").toString(
+      "utf8"
+    );
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.originalname.split(".")[0] +
+        uniqueSuffix +
+        "." +
+        file.originalname.split(".")[1]
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const config = {
     host: 'localhost',
@@ -253,6 +275,19 @@ app.get('/api/admin/getIssueRequest', verifyAdminToken, jsonParser, (request, re
   });
 });
 
+app.get('/api/getNews', jsonParser, (request, response) => {
+    const query = `SELECT * FROM public."News";`;
+
+    pool.query(query, (err, res) => {
+        if(err){
+          console.log(err);
+          response.status(404);
+          return;
+        }
+        response.status(200).send(res.rows);
+    });
+});
+
 
 function verifyToken(request, response, next) {
     const header = request.headers["authorization"];
@@ -310,6 +345,7 @@ function verifyAdminToken(request, response, next) {
     }
   }
 
+
 function main() {
     try{
         pool.connect();
@@ -319,5 +355,9 @@ function main() {
         return console.log(err);
     }
 }
+
+app.get("/api/public/*", (req, res, next) => {
+  res.sendFile("uploads/" + req.path.substring(12).replace('%20', ' '), { root: __dirname });
+});
 
 main();
